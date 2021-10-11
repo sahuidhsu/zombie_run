f4a814c2d21392be4044758d32dae6cfa3436c3e
sahuidhsu <68549547+sahuidhsu@users.noreply.github.com>
2021-10-11 13:35:27 +0800

    Part1-3

---
+++ decryption.py
@@ -0,0 +1,13 @@
+plaintext = input("Ciphertext:")
+cipherText = ""
+alphabet = "XYZABCDEFGHIJKLMNOPQRSTUVWXYZABC"
+plaintextPosition = 0
+while plaintextPosition < len(plaintext):
+    plaintextChar = plaintext[plaintextPosition]
+    alphabetPosition = 3
+    while plaintextChar != alphabet[alphabetPosition]:
+        alphabetPosition += 1
+    alphabetPosition += 3
+    cipherText = cipherText + alphabet[alphabetPosition]
+    plaintextPosition += 1
+print(cipherText)
