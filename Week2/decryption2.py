4f9d449b0b1d31a659f5d34360cf0f45c3c38198
sahuidhsu <68549547+sahuidhsu@users.noreply.github.com>
2021-10-11 14:03:09 +0800

    complete Week2

---
+++ Week2/decryption2.py
@@ -0,0 +1,10 @@
+plaintext = input("Ciphertext:")
+cipherText = ""
+plaintextPosition = 0
+while plaintextPosition < len(plaintext):
+    plaintextChar = plaintext[plaintextPosition]
+    asciiValue = ord(plaintextChar)
+    asciiValue += 3
+    cipherText = cipherText + chr(asciiValue)
+    plaintextPosition += 1
+print(cipherText)
